9839dc8f3bb1caa6dca3d33820c62e0701cf6492
Jonas Reichert <jonas.reichert@daimler.com>
2021-03-23 17:44:36 +0100

    implemented loading state

--- starter/app/src/main/java/com/udacity/DetailActivity.kt
+++ starter/app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,15 +1,26 @@
 package com.udacity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
+    private lateinit var statusTextView: TextView
+    private lateinit var fileNameTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        statusTextView = findViewById(R.id.statusTextView)
+        fileNameTextView = findViewById(R.id.fileNameTextView)
+
+        statusTextView.text = intent.getStringExtra("DOWNLOAD_STATUS")
+        fileNameTextView.text = intent.getStringExtra("FILE_NAME")
     }
 
 }
--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+import android.animation.TimeInterpolator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
@@ -8,6 +9,9 @@ import android.graphics.Rect
 import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
+import android.widget.Button
 import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
@@ -25,8 +29,40 @@ class LoadingButton @JvmOverloads constructor(
     private val buttonText = resources.getString(R.string.button_name)
     private var textBounds = Rect()
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    // progress value when loading
+    // between 0.0 and 1.0
+    private var progress: Float = 0.0F
 
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+        if (buttonState == ButtonState.Initial || buttonState == ButtonState.Completed) {
+            reset()
+        } else if (buttonState == ButtonState.Loading) {
+            startAnimator()
+            isClickable = false
+        }
+    }
+
+    private fun startAnimator() {
+        valueAnimator.setFloatValues(0.0F, 1.0F)
+        valueAnimator.duration = 2300
+        valueAnimator.interpolator = DecelerateInterpolator()
+        valueAnimator.addUpdateListener { animator ->
+            progress = animator.animatedValue as Float
+            invalidate()
+        }
+
+        valueAnimator.start()
+    }
+
+    private fun reset() {
+        progress = 0.0F
+        isClickable = true
+        cancelAnimator()
+        invalidate()
+    }
+
+    private fun cancelAnimator() {
+        TODO("Not yet implemented")
     }
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -48,13 +84,9 @@ class LoadingButton @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        paint.color = backgroundColorValue
-        canvas?.drawRect(0.0F, 0.0F, widthSize.toFloat(), heightSize.toFloat(), paint)
-
-        paint.color = textColorValue
-
-        paint.getTextBounds(buttonText, 0, buttonText.length, textBounds);
-        canvas?.drawText(buttonText, (widthSize / 2).toFloat(), (heightSize / 2 - textBounds.exactCenterY()), paint)
+        canvas?.save()
+        drawButton(ButtonState.Initial, canvas)
+        canvas?.restore()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -68,6 +100,32 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
+    }
+
+    private fun drawButton(state: ButtonState, canvas: Canvas?) {
+        if (state == ButtonState.Initial || state == ButtonState.Completed) {
+            paint.color = backgroundColorValue
+            canvas?.drawRect(0.0F, 0.0F, widthSize.toFloat(), heightSize.toFloat(), paint)
+
+            drawButtonText(canvas)
+        } else if (state == ButtonState.Loading) {
+            canvas?.drawRect(0.0F, 0.0F, progress * width.toFloat(), height.toFloat(), paint)
+
+            drawButtonText(canvas)
+            drawCircle(canvas)
+        }
+    }
+
+    private fun drawButtonText(canvas: Canvas?) {
+        paint.color = textColorValue
+        paint.getTextBounds(buttonText, 0, buttonText.length, textBounds);
+        canvas?.drawText(buttonText, (widthSize / 2).toFloat(), (heightSize / 2 - textBounds.exactCenterY()), paint)
+    }
+
+    private fun drawCircle(canvas: Canvas?) {
+        val textBounds = paint.getTextBounds(buttonText, 0, buttonText.length, textBounds);
+
+        //canvas.drawArc()
     }
 
 }
